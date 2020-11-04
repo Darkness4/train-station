ae5905352688f358b24452be6441a3c75735ce30
nguyen_marc <nguyen_marc@live.fr>
2020-11-04 19:41:56 +0100

    Github CI (#2)
    * [github ci] Added workflow
    
    * [github ci] Android fixes
    
    * [github ci] Fix nest CI

--- train-station-app/app/src/main/java/com/example/trainstationapp/presentation/ui/MainActivity.kt
+++ train-station-app/app/src/main/java/com/example/trainstationapp/presentation/ui/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.trainstationapp.presentation.ui
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trainstationapp.R
 
