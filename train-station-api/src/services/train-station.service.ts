48d73e687c337e541c77785d3e4e52d43ac6ebbb
Nguyen Marc <nguyen_marc@live.fr>
2020-11-05 16:13:29 +0100

    [train-station.service] Using observer

--- train-station-api/src/services/train-station.service.ts
+++ train-station-api/src/services/train-station.service.ts
@@ -3,41 +3,48 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm/lib/typeorm-crud.service';
 import { Station } from 'models/station';
 import { Repository } from 'typeorm';
+import { mergeMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Injectable()
 export class TrainStationService extends TypeOrmCrudService<Station> {
+  subscription?: Subscription;
+
   constructor(
     client: HttpService,
     @InjectRepository(Station) repo: Repository<Station>,
   ) {
     super(repo);
 
-    client
+    // Initially downloading the data.
+    this.subscription = client
       .get<Station[]>(
         'https://ressources.data.sncf.com/explore/dataset/liste-des-gares/download/?format=json',
       )
-      .toPromise()
-      .then((response) => {
-        if (response.status == 200) {
-          return Promise.all(response.data.map((e) => repo.save(e)));
-        } else {
-          Logger.error(
-            "Couldn't download the initial data.",
-            'TrainStationService',
-          );
-        }
-      })
-      .then((array) =>
-        Logger.log(
-          `Database initialized. Total: ${array.length} rows.`,
-          'TrainStationService',
+      .pipe(
+        mergeMap((response) =>
+          Promise.all(response.data.map((e) => repo.save(e))),
         ),
       )
-      .catch((reason) =>
-        Logger.error(
-          `Couldn't download the initial data. ${reason}`,
-          'TrainStationService',
-        ),
+      .subscribe(
+        (array) =>
+          Logger.log(
+            `Database initialized. Total: ${array.length} rows.`,
+            'TrainStationService',
+          ),
+        (reason) =>
+          Logger.error(
+            `Couldn't download the initial data. ${reason}`,
+            'TrainStationService',
+          ),
       );
+  }
+
+  async testA(): Promise<number> {
+    return 32;
+  }
+
+  async do() {
+    this.testA();
   }
 }
