d8b689fcc06f948554f1f3124c7c63f75dd9f7d4
nguyen_marc <nguyen_marc@live.fr>
2020-11-28 03:08:24 +0100

    Api update (#7)
    * [api] Changed API endpoints
    
    * [api] Documented

--- train-station-api/src/entities/fields.ts
+++ train-station-api/src/entities/fields.ts
@@ -59,7 +59,6 @@ export class Fields {
 
   @ApiProperty()
   @OneToOne(() => Geometry, {
-    eager: true,
     cascade: true,
     nullable: true,
   })
--- train-station-api/src/modules/train-station.module.ts
+++ train-station-api/src/modules/train-station.module.ts
@@ -1,7 +1,7 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { TrainStationController } from 'controllers/train-station.controller';
-import { Station } from 'models/station';
+import { Station } from 'entities/station';
 import { TrainStationService } from 'services/train-station.service';
 
 @Module({
