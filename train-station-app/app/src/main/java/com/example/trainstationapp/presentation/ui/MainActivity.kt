6cfb4a2aa4e5cf6d1bcdbf2135d0be6cec6b2626
Nguyen Marc <nguyen_marc@live.fr>
2020-12-25 02:43:17 +0100

    [navigation] Replaced startActivity with Navigation Component

--- train-station-app/app/src/main/java/com/example/trainstationapp/presentation/ui/MainActivity.kt
+++ train-station-app/app/src/main/java/com/example/trainstationapp/presentation/ui/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.trainstationapp.presentation.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.trainstationapp.R
 import com.example.trainstationapp.databinding.ActivityMainBinding
-import com.example.trainstationapp.domain.entities.Station
 import com.example.trainstationapp.domain.repositories.StationRepository
 import com.example.trainstationapp.presentation.ui.adapters.MainPagerViewAdapter
 import com.example.trainstationapp.presentation.ui.fragments.AboutFragment
-import com.example.trainstationapp.presentation.ui.fragments.StationListFragment
 import com.example.trainstationapp.presentation.viewmodels.MainViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         binding.pager.adapter = MainPagerViewAdapter(
             this,
             listOf(
-                StationListFragment.newInstance(),
+                NavHostFragment.create(R.navigation.station_list_nav_graph),
                 AboutFragment.newInstance()
             )
         )
@@ -55,21 +53,6 @@ class MainActivity : AppCompatActivity() {
                 else -> throw RuntimeException("No fragment here.")
             }
         }.attach()
-
-        // Watch for the "show details" action
-        viewModel.showDetails.observe(this) {
-            it?.let {
-                startDetailsActivity(it)
-                viewModel.showDetailsDone()
-            }
-        }
-    }
-
-    private fun startDetailsActivity(station: Station) {
-        val intent = Intent(this, DetailsActivity::class.java).apply {
-            putExtra(DetailsActivity.STATION_MESSAGE, station)
-        }
-        startActivity(intent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
