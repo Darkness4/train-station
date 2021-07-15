7859b23ed4a85206d4fc16869d25d0175280156f
Nguyen Marc <nguyen_marc@live.fr>
2021-07-16 00:13:13 +0200

    [svelte] Package

--- train-station-front/svelte.config.js
+++ train-station-front/svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +15,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter(),
 		vite: {
 			resolve: {
 				alias: {
