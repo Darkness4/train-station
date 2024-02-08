420d7d902be5ad1c8d226241454917512ce1e7c6
Nguyen Marc <nguyen_marc@live.fr>
2024-02-08 02:16:02 +0100

    chore(app): bump dependencies

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -126,7 +126,7 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
         freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
     }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.5" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.9" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
     testOptions { unitTests.all { it.useJUnitPlatform() } }
 }
