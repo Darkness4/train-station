eea5d29856bcdc86d6d3f7512060176a2e6af113
Nguyen Marc <nguyen_marc@live.fr>
2024-12-08 03:43:33 +0100

    fix(vite): use modern-compiler

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,13 @@ import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
 	plugins: [sveltekit(), imagetools()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler'
+			}
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
