957da95ca699cec181a9cc045c3c002d6165814f
Nguyen Marc <nguyen_marc@live.fr>
2024-12-08 03:39:42 +0100

    fix(front): fix vite config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
-import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit(), imagetools()],
