25bd4296b3a8fef9c13bdf18b7b31b3e3b2f2654
Marc Nguyen <nguyen_marc@ik.me>
2026-05-03 17:35:58 +0200

    fix(front): dont use front for callback

--- front/src/routes/oauth2/[...path]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const trailingSlash = 'ignore';
-
-export async function load({ url }) {
-	redirect(308, `com.example.trainstationapp://oauth2/?${url.searchParams.toString()}`);
-}
